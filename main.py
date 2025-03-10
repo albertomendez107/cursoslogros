9967e31b25e725d115d05c07723dafec9e474b02
albertomendez107 <albertomendezv2006@gmail.com>
2025-03-10 11:58:43 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+nombre = "Alberto"
+ciudad = "maracaibo"
+edad = "18"
+print(f"su nombre es", nombre,"el es de", ciudad ,"tiene", edad,)
+
+
+matematica =  "matematica 19"
+castellano = "castellano 17"
+quimica = "quimica 18"
+
+print("materia" , matematica,castellano,quimica)
+print
