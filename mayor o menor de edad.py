22825a3abdefc5815eb9a5eaba673eba775bb2f4
albertomendez107 <albertomendezv2006@gmail.com>
2025-03-16 14:36:57 -0400

    Add files via upload

---
+++ mayor o menor de edad.py
@@ -0,0 +1,8 @@
+
+# determine si una persona es mayor de edad solicitando un dato
+print(ingrese la edad de la persona)
+edad = int(input)
+if edad >= 18:
+    print("la persona es mayor de edad")
+    else:
+        print("la persona no es mayor de edad")
