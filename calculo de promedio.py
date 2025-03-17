a37be8d9ba53c2bdd4de113467f9050aa56d3df2
albertomendez107 <albertomendezv2006@gmail.com>
2025-03-17 15:00:50 -0400

    Add files via upload

---
+++ calculo de promedio.py
@@ -0,0 +1,12 @@
+
+# calculo de promedio maximo y minimo de calificaciones
+
+n = int(input("ingrese la cantidad de notas a promediar"))
+suma=0
+while(1<= n):
+    print("ingrese la nota numero:",i)
+    nota = float(input)
+    suma=suma+nota
+    i+=1
+    prom = suma / n 
+    print("el promedio de notas es:",prom)
