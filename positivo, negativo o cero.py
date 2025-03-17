adc5fc05ed91d9b548ab571c6ff1929d79ca22df
albertomendez107 <albertomendezv2006@gmail.com>
2025-03-17 12:31:34 -0400

    Add files via upload

---
+++ positivo, negativo o cero.py
@@ -0,0 +1,10 @@
+
+# "crear un programa en python para pedir un numero e indicar si es positivo, negativo o cero"
+print("ingrese un numero para verificar:")
+n = int(input())
+if(n>0)
+print("el numero ingresado es positivo")
+elif(n<0):
+    print("el numero ingresado es negativo")
+    else:
+        print("el numero ingresado es igual a 0")
