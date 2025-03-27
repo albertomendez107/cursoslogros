a6cc35bd7b296fcb8cde709b23445390c10cf9e1
albertomendez107 <albertomendezv2006@gmail.com>
2025-03-27 10:20:36 -0400

    Add files via upload

---
+++ bucle for lista.py
@@ -0,0 +1,7 @@
+# crea una lista con 5 nombres, utiliza un bucle for para cada nombre en la lista
+
+nombres = ("ana", "luis", "carlos", "sofia", "maria")
+
+# usar un bucle for para imprimir cada nombre en la lista
+for nombre in nombres:
+    print(nombre)
