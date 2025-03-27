511d9f6ee605c1c96e9ddf34c66647fb2375de3f
albertomendez107 <albertomendezv2006@gmail.com>
2025-03-27 09:51:13 -0400

    Add files via upload

---
+++ operaciones y tipos de datos.py
@@ -0,0 +1,15 @@
+# escribe un programa que solicite al usuario dos numeros, los sume, reste, multiplique y divida. luego, muestra los resultados y el tipo de dato de cada resultado
+
+
+print("ingrese el primer numero: ")
+n1 = float(input())
+print("ingrese el segundo numero: ")
+n2 = float(input())
+suma = n1 + n2
+resta = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+print("la suma es: ",suma)
+print("la resta es:", resta)
+print("la multiplicacion es: ",mult)
+print("la division es: ", div)
